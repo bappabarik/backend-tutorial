5a73479c12c2075f9706f44fbd32cd41569d8533
Bappa Barik <61709328+bappabarik@users.noreply.github.com>
2024-11-22 22:40:53 +0530

    Custom classes added to handle api error and response

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
@@ -8,6 +9,20 @@ dotenv.config({
 
 
 connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("ERROR: ", error);
+        throw error 
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port: ${process.env.PORT}`);
+        
+    })
+})
+.catch(error => {
+    console.log("Mongo DB connection failed !!! ", error);
+})
 
 
 
