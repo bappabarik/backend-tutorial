0f7c72a654330f7e73e83b9f885ec8023164a154
Bappa Barik <61709328+bappabarik@users.noreply.github.com>
2024-12-07 02:43:23 +0530

    Fix: all register controllers bugs

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
 
 
@@ -16,7 +16,7 @@ connectDB()
     })
 
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`⚙️ Server is running at port: ${process.env.PORT}`);
+        console.log(`⚙️  Server is running at port: ${process.env.PORT}`);
         
     })
 })
